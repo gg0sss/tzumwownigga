0ad97e505103178c1bd7bccbf6586c7631793bd1
gg0sss <gurovgeorge@gmail.com>
2026-01-20 11:22:58 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -134,13 +134,17 @@ else:
 new_products = {}
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--disable-background-networking")
 chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("user-agent=Mozilla/5.0")
-
+chrome_options.add_argument("--single-process")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
 try:
     send("Ищу дорогую ненужную хуйню🥶")
     driver = webdriver.Chrome(options=chrome_options)
